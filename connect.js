fc1d07e319996c9d251db42f568852b92b29d6d0
shyam9493 <shyamnandapuneedi123@gmail.com>
2024-07-25 22:04:01 +0530

    initial

---
+++ connect.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+
+const mongoUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+console.log(mongoUrl);
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+  process.exit(1); 
+});
+
+db.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+db.on('disconnected', () => {
+  console.log('Disconnected from MongoDB');
+});
+
+process.on('SIGINT', async () => {
+  try {
+    await mongoose.connection.close();
+    console.log('MongoDB connection closed due to application termination');
+    process.exit(0);
+  } catch (error) {
+    console.error('Error closing MongoDB connection:', error);
+    process.exit(1);
+  }
+});
+
+module.exports = mongoose;
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-con
+const app=require('express')();
+const dotenv=require("dotenv");
+const conn=require("./connect");
+dotenv.config();
+const port=process.env.PORT;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
